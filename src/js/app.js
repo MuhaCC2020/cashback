202b8463fc0cfa07c9f32d25ce405a36fee16c6d
Student <student@localhosts>
2020-01-07 11:11:16 +0500

    cashback test

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,17 @@
-const regularPurchases = 2000;
-const regularPurchasesPercent = 0.01;
+const  = 100;
+const regularrPurchasesPercent = 0.01;
+const increatPurchases = 100;
+const increatPurchasesPercent = 0.5;
+
+
+const spesialPurchases = 100;
+const spesialPurchasesPercent = 0.3;
+
 const result = regularPurchases * regularPurchasesPercent;
-console.log(result);
+let cashback = regularPurchases * regularPurchasesPercent;
+cashbacklimit = 3000;
+
+if (cashback = >cashbacklimit) {
+    // cashback = 3000
+}
+console.log(cashback);
